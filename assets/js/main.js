aef2ed25f7f59fa0a9f92217389f73b71ac141cf
Tommy Qu <tommyqu03@gmail.com>
2024-03-19 14:18:12 -0400

    added a section for scheduler

--- assets/js/main.js
+++ assets/js/main.js
@@ -260,7 +260,7 @@
   const portfolioDetailsLightbox = GLightbox({
     selector: '.portfolio-details-lightbox',
     width: '90%',
-    height: '90vh'
+    height: '90vh',
   });
 
   /**
@@ -268,7 +268,7 @@
    */
   new Swiper('.portfolio-details-slider', {
     speed: 400,
-    loop: true,
+    // loop: true,
     autoplay: {
       delay: 5000,
       disableOnInteraction: false
